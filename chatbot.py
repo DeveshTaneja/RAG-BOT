f87217760664355d0b23d0f8b218091fe6d2244f
DeveshTaneja <76648313+DeveshTaneja@users.noreply.github.com>
2025-04-19 15:47:16 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -24,7 +24,7 @@ def load_pdfs_from_folder(folder_path):
 
 st.title("CYBER SECURITY STUDY ASSISTANT - BY FIELD CISO ADVISORY")
 
-folder_path = r"C:\Users\Devesh\Downloads\RAG-ChatBot-With-Gemini-main\RAG-ChatBot-With-Gemini-main\files"
+folder_path = r"\files"
 # Load and read the PDF file "linear.pdf" using PyPDFLoader.
 # loader = PyPDFLoader(load_pdfs_from_folder(folder_path))
 data = load_pdfs_from_folder(folder_path)
